dbb6d858895e7fa512d29e311f38ad656bbfb0d7
Gabi Geollegue <gabigeollegue@gmail.com>
2019-09-25 22:03:07 +0000

    used constants for things that dont change

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
