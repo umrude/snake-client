d8ce39a8265cbf92b29c0fc2d5a4b3de2ca9da7f
Gabi Geollegue <gabigeollegue@gmail.com>
2019-09-25 19:48:10 +0000

    refactored play.js

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', () => {
+    console.log("you ded cuz you idled");
+  });
+  return conn;
+};
+
+module.exports = connect;
