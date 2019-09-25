0acee73dccbcdf05349901a84fb14484560128db
Gabi Geollegue <gabigeollegue@gmail.com>
2019-09-25 20:16:53 +0000

    learned about the move keys

--- client.js
+++ client.js
@@ -10,8 +10,8 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: GGG");
+    console.log("Successfully connected to game server!");
+    conn.write("Name: Gab");
   });
   conn.on('data', () => {
     console.log("you ded cuz you idled");
@@ -20,3 +20,8 @@ const connect = function() {
 };
 
 module.exports = connect;
+
+// "Move: up" - move up one square(unless facing down)
+// "Move: down" - move down one square(unless facing up)
+// "Move: left" - move left one square(unless facing right)
+// "Move: right" - move left one square(unless facing left)
