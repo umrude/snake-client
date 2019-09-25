e5b884ae00164ecbd1df53b786f4eb23d2a9f133
Gabi Geollegue <gabigeollegue@gmail.com>
2019-09-25 21:12:36 +0000

    added the ability to read user input and exit out

---
+++ input.js
@@ -0,0 +1,19 @@
+let handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+console.log('Connecting ...');
+
+
+
+module.exports = setupInput;
